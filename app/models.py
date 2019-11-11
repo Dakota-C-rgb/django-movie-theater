6eaf80ff319cb21301e1529f71d016f8ae4b0f74
Base Camp <basecamp@Bases-MacBook-Pro.local>
2019-11-11 12:57:06 -0600

    Created the models: Movie, Showing, Ticket and created the url's that interact with the views

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.TextField()
+    rating = models.FloatField()
+    genre = models.TextField()
+    runtime = models.DateField()
+
+class Showing(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    showtime = models.DateTimeField()
+
+class Ticket(models.Model):
+    name = models.TextField()
+    purchased_at = models.DateField()
+    showing = models.ForeignKey(Showing, on_delete=models.PROTECT)
+ 
